97640fba29cb61c8e48aef689df7df79bbc18415
noecassam <cassamnoe@gmail.com>
2025-07-18 10:08:35 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Crée la carte
+const map = L.map('map').setView([46.5, 2.5], 6);  // Coordonnées centrées sur la France
+
+// Fond de carte OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Liste de tes randonnées
+const randos = [
+    {
+        nom: "GR 49 - 5J - 08/2024",
+        fichier: "gpx/gr49.gpx",
+        couleur: "green"
+    },
+];
+
+// Charge chaque fichier GPX
+randos.forEach(rando => {
+    new L.GPX(rando.fichier, {
+        async: true,
+        polyline_options: {
+            color: rando.couleur
+        }
+    }).on('loaded', function(e) {
+        map.fitBounds(e.target.getBounds());
+    }).addTo(map);
+});
